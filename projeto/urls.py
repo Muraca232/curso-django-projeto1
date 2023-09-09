69ea866ed02846312dd268db004a84645fd3a30e
Nathanael Carvalho <nathanaelcarvalho777@gmail.com>
2023-09-09 01:29:11 -0300

    new

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+# HTTP request
+def my_view(request):
+    return HttpResponse('Uma Linda String')
+    # HTTP response
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
